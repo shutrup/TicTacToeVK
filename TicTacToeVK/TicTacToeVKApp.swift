7136a43e4bf3b96080723d4c913b856ec0c3c48f
shutrup <magagaraev95@mail.ru>
2024-04-06 11:33:34 +0300

    Initial Commit

---
+++ TicTacToeVK/TicTacToeVKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeVKApp.swift
+//  TicTacToeVK
+//
+//  Created by Шарап Бамматов on 06.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeVKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
